9821f546220d4d1c2e5409ddd2e5005176edbdf3
lucas-alexandrino <lucas.alexcontato@gmail.com>
2023-01-22 18:49:24 +0000

    Exercicio1

---
+++ modulo-01_fundamentos/03-introducao_a_javascript/01-javascript_primeiros_passos/scripts.js
@@ -0,0 +1,12 @@
+const myName ="Lucas";
+const birthCity ="São Paulo";
+let birthYear = 2003;
+
+
+
+//console.log(myName,birthCity,birthYear);
+
+birthYear = 2030
+birthCity = "Rio de Janeiro"
+
+console.log(myName,birthCity,birthYear);
